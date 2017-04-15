2df940c47fe3cba097c3af2eaa094f8c662c82c4
p56f <pawelcc@o2.pl>
2017-04-15 19:20:36 +0200

    Small style fix

--- src/main/scala/pl/pecet/wordsanalyzer/WordsAnalyzer.scala
+++ src/main/scala/pl/pecet/wordsanalyzer/WordsAnalyzer.scala
@@ -21,7 +21,7 @@ object WordsAnalyzer extends App {
 
   def findEpisodeDialogs(season: String, episode: Int) = {
     val url = f"http://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=friends&episode=${season}e$episode%02d"
-    val html = Source.fromURL(url, StandardCharsets.UTF_8.name())
+    val html = Source.fromURL(url, StandardCharsets.UTF_8.name)
     val divPattern = """(?s)<div class="scrolling-script-container">(.*?)</div>""".r
     divPattern.findFirstMatchIn(html.mkString) match {
       case Some(m) => m.group(1).trim
